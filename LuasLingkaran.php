e97a5e03fe89f666e78b7a36a3a0655735545dd4
rvnwr <rivanrisalah11@gmail.com>
2025-10-30 21:50:28 +0700

    Update pertemuan-5(Constructor)

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -3,11 +3,23 @@
 class LuasLingkaran {
     public const phi = 3.14;
     public int $jari;
+
+    public function __construct($isijari = 1){
+        $this->jari = $isijari; 
+    }
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
+        echo "Lingkaran {$nama} Hasilnya adalah:{$rumus}";
+    }
+    public static function testing(){
+        echo "ini dari static";
+    }
+    public function __destruct(){
+        echo "Udah ah cape";
+    }
 }
 
-$Lingkaran = new LuasLingkaran();
-$Lingkaran -> jari = 16;
+$Lingkaran = new LuasLingkaran(16);
+$Lingkaran -> tampil('roda');
 
-$rumus = LuasLingkaran::phi * $Lingkaran->jari * $Lingkaran->jari;
-
-echo "Hasilnya adalah: ". $rumus;
+LuasLingkaran::testing();
